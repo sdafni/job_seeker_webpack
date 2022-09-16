2aee05758ce515fae8485d55c939ebf659b43430
Your Name <you@example.com>
2022-09-16 21:22:23 +0300

    routes#2

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import { BrowserRouter as HashRouter, Route, Routes } from 'react-router-dom'
+import { HashRouter, Route, Routes } from 'react-router-dom'
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+
+
+
+  
      <HashRouter>
+            <Routes>
+
         <Route path="/" element={<App />}/>
         <Route path="/content_script" element={<App content_view={true}/>}/>
+        </Routes>
+
       </HashRouter>
     
-  </React.StrictMode>
+
+
 ) 
